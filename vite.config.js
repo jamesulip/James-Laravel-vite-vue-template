f52f11b3829860063e9c05af7994a6d6a4fb5552
James Ulip <ulip.james@gmail.com>
2023-10-03 14:32:00 +0800

    vue-router

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
 
 export default defineConfig({
     plugins: [
+        VueRouter({
+            routesFolder: 'resources/js/pages',
+          }),
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
         }),
     ],
